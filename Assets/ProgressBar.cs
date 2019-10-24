39adfafe2b0aecebc9f9de272c423cfd5e76daeb
Alex B <alexander.burnett6@gmail.com>
2019-10-24 12:16:56 +0100

    Added progressbar and task logic
    Progress bar now updates based on player progress and aim.
    
    Tasks can now be removed when ticked, However points are only given if the iputfield was filled.
    
    New tasks can be added when no tasks exist on screen

---
+++ Assets/ProgressBar.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+
+    public const string KEY_PROGRESS = "Progress";
+    public const string KEY_PROGRESSAIM = "ProgressAim";
+    public float Progress
+    {
+        get { return PlayerPrefs.GetFloat(KEY_PROGRESS);  }
+        set { PlayerPrefs.SetFloat(KEY_PROGRESS, value); }
+    }
+    public float ProgressAim
+    {
+        get { return PlayerPrefs.GetFloat(KEY_PROGRESSAIM); }
+        set { PlayerPrefs.SetFloat(KEY_PROGRESSAIM, value); }
+    }
+    [SerializeField]
+    public Image ProgressFill;
+    private float AimMultiplier = 1.5F;
+    public Text ProgressText;
+    private string ProgressTextString;
+    public bool UseDefaults;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (UseDefaults == true)
+        {
+            Progress = 0;
+            ProgressAim = 20;
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+
+        //Controls the fill of the progress bar based on the player's progress and progress aim
+        ProgressFill.fillAmount = Progress / ProgressAim;
+
+        //Formats the progress bar text to remain updated with the current progress and aim of the player
+        ProgressTextString = string.Format(Progress+"/"+ProgressAim);
+        ProgressText.GetComponent<Text>().text = ProgressTextString;
+
+        //Once player has reached their progress aim, the aim is updated via multiplication.
+        if (Progress >= ProgressAim)
+        {
+            PlayerPrefs.SetFloat("ProgressAim", ProgressAim * AimMultiplier);
+        }
+    }
+}
---
+++ Assets/Task.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class Task : MonoBehaviour
+{
+    [SerializeField]
+    private int TaskPointValue = 2;
+    private string InputFieldText;
+    public GameObject TaskElementBackground;
+    public Text TaskTextRef;
+    public InputField TaskInputField;
+    private IEnumerator coroutine;
+    private Button button;
+    public Color colorToAnimateBackgroundTo;
+
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        //Change the task background tint to green to indicate it has been completed
+        TaskElementBackground.GetComponent<Image>().color = colorToAnimateBackgroundTo;
+        //Change text colour to white so that it is more visible against the green tint
+        TaskTextRef.GetComponent<UnityEngine.UI.Text>().color = Color.white;
+        //Set task text to "+{points} Points!" to indicate to the player that they have earned points for completing task
+        TaskTextRef.GetComponent<UnityEngine.UI.Text>().text = "+" + TaskPointValue + " points!";
+
+        //Increases the player's progress value by the value of the task they have just completed, IF, a task of more than 0 character was inputted.
+        InputFieldText = TaskInputField.text;
+        if (InputFieldText.Length > 0)
+        {
+            PlayerPrefs.SetFloat("Progress", PlayerPrefs.GetFloat("Progress", 0) + TaskPointValue);
+        }
+
+        //Testing OnClick 
+        Debug.Log("Test");
+
+        //Starts the Coroutine after 2 seconds
+        coroutine = WaitAndDestroy(2.0f);
+        StartCoroutine(coroutine);
+        //StartCoroutine(WaitAndDestroy(2.0f));
+
+    }
+
+    private IEnumerator WaitAndDestroy(float waitTime)
+    {
+        //Wait for 2 Seconds before executing
+        yield return new WaitForSeconds(waitTime);
+
+        //Destroy the parent object of the task button, aka. the task element itself. 
+        Destroy(transform.parent.gameObject);
+    }
+}
